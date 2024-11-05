7732f845881107b0cb048bc629bbecbbf1b0a510
jastit00 <jastit00@hs-esslingen.de>
2024-11-05 22:25:57 +0100

    css

--- home.php
+++ home.php
@@ -31,10 +31,12 @@
       <button type="button" class ="statsbutton"><a href="notifications.html">notifications</a></button>
     </div>
     
+   
 
-
-    <div class="vacationbox">
-            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST" class="vacationform">
+    <div class="vacationform">
+    <p class="calp">book day</p>
+    <br>
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST" class="vacationbox">
                 Date: <input type="date" name="date" class="vacainput" required>
                 Begin: <input type="time" name="time_started" class="vacainput" required> 
                 End: <input type="time" name="time_ended" class="vacainput" required>
