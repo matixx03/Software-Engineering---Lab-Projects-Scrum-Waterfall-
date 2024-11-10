736b38f71271b240430a5906eacfcf0b3843e528
jastit00 <jastit00@hs-esslingen.de>
2024-11-10 17:36:32 +0100

    stat

--- statistics.php
+++ statistics.php
@@ -9,8 +9,8 @@
     <nav class="navigation">
       <div class="navlist">
         <ul>
-          <li class="navelement"><a href="index.html" class="navlink">Home</a></li>
-          <li class="navelement"><a href="vacation.html" class="navlink">Vacation Planner</a> </li>
+          <li class="navelement"><a href="home.php" class="navlink">Home</a></li>
+          <li class="navelement"><a href="vacation.php" class="navlink">Vacation Planner</a> </li>
           <li class="navelement"><a href="sickdays.html" class="navlink">Sickday Planner</a> </li>
         </ul>
       </div>
@@ -33,11 +33,11 @@
         die("Connection failed: " . $conn->connect_error);
     }
 
-    $sql = "SELECT time_started, -
+    $sql = "SELECT time_started, 
                    CASE WHEN time_started > 8 THEN time_started - 8 ELSE 0 END AS overtime_hours,
                    DATE_FORMAT(CURRENT_DATE, '%Y-%m-%d') AS current_date, 
                    DAYNAME(CURRENT_DATE) AS weekday 
-            FROM time_management";
+            FROM day";
     $result = $conn->query($sql);
 
    
@@ -70,16 +70,7 @@
             </tr>
           </thead>
           <tbody>
-            <tr><td>Eintrag 1</td><td>Eintrag 2</td><td>Eintrag 3</td></tr>
-            <tr><td>Eintrag 4</td><td>Eintrag 5</td><td>Eintrag 6</td></tr>
-            <tr><td>Eintrag 7</td><td>Eintrag 8</td><td>Eintrag 9</td></tr>
-            <tr><td>Eintrag 10</td><td>Eintrag 11</td><td>Eintrag 12</td></tr>
-            <tr><td>Eintrag 13</td><td>Eintrag 14</td><td>Eintrag 15</td></tr>
-            <tr><td>Eintrag 16</td><td>Eintrag 17</td><td>Eintrag 18</td></tr>
-            <tr><td>Eintrag 19</td><td>Eintrag 20</td><td>Eintrag 21</td></tr>
-            <tr><td>Eintrag 22</td><td>Eintrag 23</td><td>Eintrag 24</td></tr>
-            <tr><td>Eintrag 25</td><td>Eintrag 26</td><td>Eintrag 27</td></tr>
-            <tr><td>Eintrag 28</td><td>Eintrag 29</td><td>Eintrag 30</td></tr>
+            
           </tbody>
         </table>
     </div>
