4dce3386c1dd007cf7758f3732835a73b0426784
Akimk00 <akimk00@gmail.com>
2024-11-22 17:38:25 +0100

    Added add/delete media

--- scrum project/Manage_Catalog_PHP.php
+++ scrum project/Manage_Catalog_PHP.php
@@ -19,26 +19,28 @@
     <h1 class="header1">Add book</h1>
     <main> 
     <div class="add_book">
-    <form action="Manage_Catalogp.php" method="POST" class="addform">
-            Title: <input type="text" name="title" class="addform" required>
-            Author: <input type="text" name="author" class="addform" required>
-            Year: <input type="number" name="year" class="addform" value="2000" required>
-            Edition: <input type="text" name="edition" class="addform" required>
-            Publisher: <input type="text" name="publisher" class="addform" required>
-            Number of Pieces: <input type="number" name="number_of_pieces" class="addform"min="1" value="1"required>
-            <input type="submit" name=" add " value="add " class="vacainput">
+    <form action="Manage_Catalog_PHP.php" method="POST" class="addform">
+            Title: <input type="text" name="Title" class="addform" required>
+            Author: <input type="text" name="Author" class="addform" required>
+            Year: <input type="number" name="Year" class="addform" value="2000" required>
+            Edition: <input type="text" name="Edition" class="addform" required>
+            Publisher: <input type="text" name="Publisher" class="addform" required>
+            Number of Pieces: <input type="number" name="Pieces" class="addform"min="1" value="1"required>
+            <input type="submit" name="add" value=" Add Book " class="vacainput">
         </form>
 </div>
 <div class="table-container">
     <table>
         <thead>
             <tr>
+                <th>ID</th>
                 <th>Title</th>
                 <th>Author</th>
                 <th>Year</th>
                 <th>Edition</th>
                 <th>Publisher</th>
                 <th>Number of Pieces</th>
+                <th>Action</th>
             </tr>
         </thead>
         <tbody>
@@ -55,23 +57,24 @@
             }
 
             if ($_SERVER["REQUEST_METHOD"] === "POST") {
-                if (isset($_POST['save'])) {
+                if (isset($_POST['add'])) {
                     // Hole die Eingabewerte
-                    $title = $_POST['title'];
-                    $author = $_POST['author'];
-                    $year = $_POST['year'];
-                    $edition = $_POST['edition'];
-                    $publisher = $_POST['publisher'];
-                    $number_of_pieces = $_POST['number_of_pieces'];
+                    $title = $_POST['Title'];
+                    $author = $_POST['Author'];
+                    $year = $_POST['Year'];
+                    $edition = $_POST['Edition'];
+                    $publisher = $_POST['Publisher'];
+                    $number_of_pieces = $_POST['Pieces'];
 
                    
                     if ($number_of_pieces < 1) {
                         echo "<p style='color: red;'>The number of pieces must be at least 1.</p>";
                     } else {
                        
-                        $sql = "INSERT INTO catalog (title, author, year, edition, publisher, number_of_pieces)
+                        $sql = "INSERT INTO book (Title, Author, Year, Edition, Publisher, Pieces)
                                 VALUES ('$title', '$author', '$year', '$edition', '$publisher', '$number_of_pieces')";
                         if ($conn->query($sql) === TRUE) {
+                            #header('Location: Manage_Catalog_PHP.php');
                             echo "<p>New book added successfully!</p>";
                         } else {
                             echo "<p style='color: red;'>Error: " . $conn->error . "</p>";
@@ -81,28 +84,29 @@
 
                 if (isset($_POST['delete'])) {
                     foreach ($_POST['delete'] as $id => $value) {
-                        $sql = "DELETE FROM catalog WHERE id = $id";
+                        $sql = "DELETE FROM book WHERE id = $id";
                         $conn->query($sql);
                     }
                 }
             }
 
             
-            $sql = "SELECT * FROM catalog ORDER BY title";
+            $sql = "SELECT * FROM book ORDER BY title";
             $result = $conn->query($sql);
 
             if ($result->num_rows > 0) {
                 while ($row = $result->fetch_assoc()) {
                     echo "<tr>";
-                    echo "<td>" . $row["title"] . "</td>";
-                    echo "<td>" . $row["author"] . "</td>";
-                    echo "<td>" . $row["year"] . "</td>";
-                    echo "<td>" . $row["edition"] . "</td>";
-                    echo "<td>" . $row["publisher"] . "</td>";
-                    echo "<td>" . $row["number_of_pieces"] . "</td>";
+                    echo "<td>" . $row["ID"] . "</td>";
+                    echo "<td>" . $row["Title"] . "</td>";
+                    echo "<td>" . $row["Author"] . "</td>";
+                    echo "<td>" . $row["Year"] . "</td>";
+                    echo "<td>" . $row["Edition"] . "</td>";
+                    echo "<td>" . $row["Publisher"] . "</td>";
+                    echo "<td>" . $row["Pieces"] . "</td>";
                     echo "<td>";
-                    echo "<form action='Manage_Catalogp.php' method='POST' style='display: inline;'>";
-                    echo "<input type='submit' value='Delete' class='delete' name='delete[" . $row["id"] . "]'>";
+                    echo "<form action='Manage_Catalog_PHP.php' method='POST' style='display: inline;'>";
+                    echo "<input type='submit' value='Delete' class='delete' name='delete[" . $row["ID"] . "]'>";
                     echo "</form>";
                     echo "</td>";
                     echo "</tr>";
